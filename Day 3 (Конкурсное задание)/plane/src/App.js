4fca98b120b54d0de858f185b9f85915955eca58
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-09-20 13:38:11 +0500

    Add files via upload

---
+++ Day 3 (Конкурсное задание)/plane/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Profile from './pages/Profile/Profile'
+import Reservation from './pages/Reservation/Reservation'
+import Search from './pages/Search/Search'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/reservation" element={<Reservation />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
+  )
+}
+
+export default App
